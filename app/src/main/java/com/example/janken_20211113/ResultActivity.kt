9ebad793cecab0aea56271fa6e0933932c293cd4
t7m3 <toda7.mk3@gmail.com>
2021-11-18 16:43:52 +0900

    11/18-2 先に、戻るボタンで前画面に戻るようにした。（遷移先でデータを取り出すのは、まだ。）

--- app/src/main/java/com/example/janken_20211113/ResultActivity.kt
+++ app/src/main/java/com/example/janken_20211113/ResultActivity.kt
@@ -2,10 +2,15 @@ package com.example.janken_20211113
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.janken_20211113.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backButton.setOnClickListener{finish()}
     }
 }
