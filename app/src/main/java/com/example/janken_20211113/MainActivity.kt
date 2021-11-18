2679a4367503ee9f8651b23661a0b256971472a3
t7m3 <toda7.mk3@gmail.com>
2021-11-18 16:05:05 +0900

    11/18-1 gu、choki、paのボタンで画面遷移する。（遷移先でデータを取り出すのは、まだ。）

--- app/src/main/java/com/example/janken_20211113/MainActivity.kt
+++ app/src/main/java/com/example/janken_20211113/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.janken_20211113
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.janken_20211113.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gu.setOnClickListener{onJankenButtonTapped(it)}
+        binding.choki.setOnClickListener{onJankenButtonTapped(it)}
+        binding.pa.setOnClickListener{onJankenButtonTapped(it)}
+    }
+
+    fun onJankenButtonTapped(view:View){
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
 }
