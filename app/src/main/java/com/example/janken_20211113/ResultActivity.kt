e5a4a70e9e731571985c5da182c823c09fcf213e
t7m3 <toda7.mk3@gmail.com>
2021-11-18 17:57:40 +0900

    11/18-4 コンピュータの手を表示した。（勝敗の表示は、まだ。）

--- app/src/main/java/com/example/janken_20211113/ResultActivity.kt
+++ app/src/main/java/com/example/janken_20211113/ResultActivity.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.janken_20211113.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    val gu = 0
+    val choki = 1
+    val pa = 2
+
+
     private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,14 @@ class ResultActivity : AppCompatActivity() {
             R.id.pa -> binding.myHandImage.setImageResource(R.drawable.pa)
 
             //else -> binding.myHandImage.setImageResource(R.drawable.pa)
+        }
+
+        // コンピュータの手を決める
+        val  comHand = (Math.random() * 3).toInt()
+        when (comHand) {
+            gu -> binding.comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> binding.comHandImage.setImageResource(R.drawable.com_choki)
+            pa -> binding.comHandImage.setImageResource(R.drawable.com_pa)
         }
 
         binding.backButton.setOnClickListener{finish()}
